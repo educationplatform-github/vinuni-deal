5f3573400e4f55bbbe27acfb5312fb705db1ad6f
educationplatform-github <168272835+educationplatform-github@users.noreply.github.com>
2024-04-29 10:49:47 +0000

    FAQ

--- pages/FAQ.py
+++ pages/FAQ.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title('FAQ - Frequently Asked Questions')
 
 faqs = [
-    ("What is your return policy?", "Our return policy is 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange."),
-    ("How do I track my order?", "You can track your order by logging into your account and visiting the 'Orders' section."),
+    ("Why Abroad in Vietnam wants to form this deal in the first place?", "Our return policy is 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange."),
+    ("What if Abroad in Vietnam can't get this deal with VinUni?", "We are gonna go with another university"),
     ("Do you ship internationally?", "Yes, we ship worldwide. Shipping costs will apply, and will be added at checkout."),
     # Add more FAQs here
 ]
